b988030fa9c4e2c06519866df52b26a050fd9663
fgonza23 <fgonza0267@gmail.com>
2018-09-02 11:16:10 -0500

    updated

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,2 +1,17 @@
 class Contact < ApplicationRecord
+
+  def friendly_updated_at
+
+    created_at.strftime("%A, %b %d")
+
+  end
+
+  def full_name
+    "#{first_name} #{last_name}"
+  end
+
+  def japan_phone_number
+    "+81 #{phone_number}"
+
+  end
 end
